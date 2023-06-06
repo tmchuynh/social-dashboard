93d299e8fc3b912c849775fdefcedfb710c239b7
tmchuynh <tinamchuynh@gmail.com>
2023-06-06 15:12:09 -0700

    update 1 file and create 1 file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './styles/global.css';
+import Header from './components/Header';
+import Switch from './components/Switch';
+import TopCardList from './components/TopCardList';
+import Overview from './components/Overview';
 
 
 function App() {
   return (
     <>
-
+      <Header>
+        <Switch/>
+      </Header>
+      <TopCardList/>
+      <Overview/>
     </>
   );
 }
